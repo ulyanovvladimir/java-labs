c92be1cf24e7c810e41c3b57be5b92cd3200479a
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2017-11-07 16:11:53 +0800

    Тесты

--- java-files/src/ru/isu/labs/files/Tests.java
+++ java-files/src/ru/isu/labs/files/Tests.java
@@ -18,7 +18,7 @@ public class Tests {
     @Test
     public void testReadFile(){
         {
-            String s = Main.readFile(FILE, "UTF-8");
+            String s = Main.readFile(FILE, "windows-1251");
             assertTrue("Успешно загружает словарь слов", s.startsWith("-де"));
             assertTrue(s.contains("йод"));
             assertTrue(s.contains("\n"));
@@ -39,8 +39,34 @@ public class Tests {
         assertFalse(Main.isWord("Аптека. Улица. Фонарь. Точка"));
         assertTrue(Main.isWord("волк"));
         assertTrue(Main.isWord("мир"));
-        assertFalse(Main.isWord("дом"));
-
-
+        assertTrue(Main.isWord("дом"));
     }
+
+    @Test
+    public void testStartsWith(){
+        int n = Main.wordsStartsWith("лев");
+        assertEquals("22 льва", 22, n);
+        n = Main.wordsStartsWith("попугай");
+        assertEquals("4 попугая", 4, n);
+        assertEquals("10 мемов", 10, Main.wordsStartsWith("мем"));
+    }
+
+    @Test
+    public void testContains(){
+        int n = Main.wordsContains("лев");
+        assertEquals("336 львов", 336, n);
+        n = Main.wordsContains("поп");
+        assertEquals("233 поп", 233, n);
+        assertEquals("305 миров", 305, Main.wordsContains("мир"));
+    }
+
+    @Test
+    public void testContWordsBySize(){
+        assertEquals("10 букв", 12544, Main.countWordBySize(10));
+        assertEquals("13 букв", 5487, Main.countWordBySize(13));
+        assertEquals("16 букв", 1409, Main.countWordBySize(16));
+        assertEquals("19 букв", 265, Main.countWordBySize(19));
+        assertEquals("22 буквы", 28, Main.countWordBySize(22));
+    }
+
 }
